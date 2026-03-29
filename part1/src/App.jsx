64011bc25dd24258ac458e560b2b791719e56a16
FourLorn <sentretluva@gmail.com>
2026-03-28 20:38:54 -0500

    part 1.11

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -6,11 +6,11 @@ const Header = (props) => <div>
 
 const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
 
-const StatisticLine = (props) => <div>
-  <li>
-      {props.text} {props.value}
-  </li>
-</div>
+const StatisticLine = (props) => 
+<tr>
+  <td>{props.text}</td>
+  <td>{props.value}</td>
+</tr>
 
 const Statistics = (props) => {
   if (props.good === 0 && props.neutral === 0 && props.bad === 0) {
@@ -23,14 +23,16 @@ const Statistics = (props) => {
     )
   } 
   return(
-    <div>
-      <StatisticLine text="good" value={props.good} />
-      <StatisticLine text="neutral" value={props.neutral} />
-      <StatisticLine text="bad" value={props.bad} />
-      <StatisticLine text="all" value={props.good + props.neutral + props.bad } />
-      <StatisticLine text="average" value={(props.good - props.bad) / (props.good + props.neutral + props.bad)} />
-      <StatisticLine text="positive" value={props.good / (props.good + props.neutral + props.bad) * 100 + " %"} />
-    </div>
+    <table>
+      <tbody>
+        <StatisticLine text="good" value={props.good} />
+        <StatisticLine text="neutral" value={props.neutral} />
+        <StatisticLine text="bad" value={props.bad} />
+        <StatisticLine text="all" value={props.good + props.neutral + props.bad } />
+        <StatisticLine text="average" value={(props.good - props.bad) / (props.good + props.neutral + props.bad)} />
+        <StatisticLine text="positive" value={props.good / (props.good + props.neutral + props.bad) * 100 + " %"} />
+      </tbody>
+    </table>
   )
 }
 
